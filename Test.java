9ba771e6b8a11cc1f3d8697300ce99f67d42ff13
bob <vlakd0711@hansung.ac.kr>
2018-06-03 21:17:36 +0900

    commit-4

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Second commit");
         System.out.println("Third commit");
+        System.out.println("this is bob's meesage");
     }
 }
